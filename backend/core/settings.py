36c3d892e806da50e692dc889e9f52d3ec804010
archanatimilsina <archanatimilsina88@gmail.com>
2026-06-17 18:44:15 +0545

    project Update

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -180,7 +180,9 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
-    'https://dairy-management-system-git-main-archanatimilsinas-projects.vercel.app'
+    'https://dairy-management-system-git-main-archanatimilsinas-projects.vercel.app',
+    'dairy-management-system-rho.vercel.app',
+    'dairy-management-system-ej2jdvxiy-archanatimilsinas-projects.vercel.app'
 ]
 
 
