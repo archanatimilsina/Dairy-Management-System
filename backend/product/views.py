30cff731e6ac26ec44008368419aca4c9856ecc0
archanatimilsina <archanatimilsina88@gmail.com>
2026-06-18 14:43:20 +0545

    project Update

--- backend/product/views.py
+++ backend/product/views.py
@@ -20,63 +20,14 @@ class ProductPagination(PageNumberPagination):
 
 
 class ProductListCreateView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
+    queryset = Product.objects.select_related('category').all()
     serializer_class = ProductSerializer
     pagination_class = ProductPagination
 
-    def create(self, request, *args, **kwargs):
-        data = request.data.copy()
-        
-        image_file = request.FILES.get('picture_src')
-        if image_file:
-            ext = image_file.name.split('.')[-1]
-            filename = f"products/{uuid.uuid4()}.{ext}"
-            file_bytes = image_file.read()
-            
-            supabase.storage.from_('media').upload(
-                path=filename,
-                file=file_bytes,
-                file_options={"content-type": image_file.content_type}
-            )
-            
-            public_url = supabase.storage.from_('media').get_public_url(filename)
-            data['picture_src'] = public_url
-
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
+    queryset = Product.objects.select_related('category').all()
     serializer_class = ProductSerializer
 
-    def update(self, request, *args, **kwargs):
-        data = request.data.copy()
-        
-        image_file = request.FILES.get('picture_src')
-        if image_file:
-            ext = image_file.name.split('.')[-1]
-            filename = f"products/{uuid.uuid4()}.{ext}"
-            file_bytes = image_file.read()
-            
-            supabase.storage.from_('media').upload(
-                path=filename,
-                file=file_bytes,
-                file_options={"content-type": image_file.content_type}
-            )
-            
-            public_url = supabase.storage.from_('media').get_public_url(filename)
-            data['picture_src'] = public_url
-
-        serializer = self.get_serializer(
-            self.get_object(), 
-            data=data, 
-            partial=kwargs.pop('partial', False)
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
 
 class CartListCreateView(generics.ListCreateAPIView):
    queryset = Cart.objects.all()
